002c2447ab4b02eae9a60b9b9afe779b0f7d43f3
Ryan Kelley <kelleyr@duvalschools.org>
2022-08-23 09:59:37 -0400

    v0.2
    Added class declaration.

--- C#/hello_world.cs
+++ C#/hello_world.cs
@@ -1,2 +1,14 @@
-// Hello, World! by Ryan Kelley, 08/23/22, 9:58AM, v0.1
+// Hello, World! by Ryan Kelley, 08/23/22, 9:58AM, v0.2
 namespace HelloWorld
+{
+    class Hello {
+
+
+
+    }
+
+
+
+
+
+}
