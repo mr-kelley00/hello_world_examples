998437d628cc630d11e3326fdc0466380ec4d2b5
Ryan Kelley <kelleyr@duvalschools.org>
2022-08-23 14:34:44 -0400

    v0.3
    Added Main method signature.

--- kelley_ryan_hello_world4.cs
+++ kelley_ryan_hello_world4.cs
@@ -1,8 +1,17 @@
-// Hello, world!, Ryan Kelley, 08/23/22, 2:04PM, v0.1 
+// Hello, world!, Ryan Kelley, 08/23/22, 2:04PM, v0.3
 using System;
 
 namespace HelloWorld
 {
+    class Hello {
+        static void Main(string[] args) 
+        {
+            
+        }
+         
+
+
+    }
 
 
 
