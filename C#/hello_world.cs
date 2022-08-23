c20e7996f39a5607be8fc570ec86f861bea2f84a
Ryan Kelley <kelleyr@duvalschools.org>
2022-08-23 10:00:32 -0400

    v0.3
    Added main() method declaration.

--- C#/hello_world.cs
+++ C#/hello_world.cs
@@ -1,8 +1,11 @@
-// Hello, World! by Ryan Kelley, 08/23/22, 9:58AM, v0.2
+// Hello, World! by Ryan Kelley, 08/23/22, 10:00AM, v0.3
 namespace HelloWorld
 {
     class Hello {
+        static void Main(string[] args)
+        {
 
+        }
 
 
     }
