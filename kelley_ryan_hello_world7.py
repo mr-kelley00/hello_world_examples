5d38fbf3d7b22be70f8d1fc75e52a73d10409286
Ryan Kelley <kelleyr@duvalschools.org>
2022-08-24 11:45:46 -0400

    v0.2
    Printed name and class period to screen.

---
+++ kelley_ryan_hello_world7.py
@@ -0,0 +1,5 @@
+# Hello, World!  Ryan Kelley, 08/22/22 12:35pm v0.2
+print("Hello, world!") # Comments don't have to start on the beginning of the line.  
+print("My name is XYZ and I am in period 7B.")
+
+
