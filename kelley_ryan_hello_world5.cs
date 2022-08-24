0baaf69c7384a4d7ae42b65be9c991e59ad7def0
Ryan Kelley <kelleyr@duvalschools.org>
2022-08-24 09:05:11 -0400

    v0.3
    Created namespace, and class declaration.

--- kelley_ryan_hello_world5.cs
+++ kelley_ryan_hello_world5.cs
@@ -1,4 +1,25 @@
-// Hello, World!, Ryan Kelley, 08/24/22, 8:43AM, version 0.1 
+// Hello, World!, Ryan Kelley, 08/24/22, 8:43AM, version 0.3 
 using System; // loads the System library.  
 
 namespace HelloWorld 
+// ()  Parentheses
+// {}  Curly Braces
+// []  Brackets 
+{
+    class Hello {
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
