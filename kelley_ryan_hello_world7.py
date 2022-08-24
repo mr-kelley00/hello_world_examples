8af6addedd558f7bcddcf2c8d99c6b2be6b7d9b6
Ryan Kelley <kelleyr@duvalschools.org>
2022-08-24 11:49:22 -0400

    v0.3
    Printed my favorite amphibian.

--- kelley_ryan_hello_world7.py
+++ kelley_ryan_hello_world7.py
@@ -1,5 +1,6 @@
-# Hello, World!  Ryan Kelley, 08/22/22 12:35pm v0.2
+# Hello, World!  Ryan Kelley, 08/24/22 11:48am v0.3
 print("Hello, world!") # Comments don't have to start on the beginning of the line.  
 print("My name is XYZ and I am in period 7B.")
+print("I like turtles.")
 
 
