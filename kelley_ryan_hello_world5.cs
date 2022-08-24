986c4b1448fc74e122b292bda4c7989ee60a6b3a
Ryan Kelley <kelleyr@duvalschools.org>
2022-08-24 08:56:17 -0400

    v0.1
    Imported System library, created HelloWorld namespace.

--- kelley_ryan_hello_world5.cs
+++ kelley_ryan_hello_world5.cs
@@ -1,1 +1,4 @@
-// Hello, World!, Ryan Kelley, 08/24/22, 8:43AM, version 0.0 
+// Hello, World!, Ryan Kelley, 08/24/22, 8:43AM, version 0.1 
+using System; // loads the System library.  
+
+namespace HelloWorld 
