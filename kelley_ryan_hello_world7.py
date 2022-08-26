4c3356e7111f6498d3a5f8633ac8c0da206c71bc
Kelley <kelleyr@duvalschools.org>
2022-08-26 12:19:22 -0400

    v0.5
    Added input() to get fav_color and then print back to screen.

--- kelley_ryan_hello_world7.py
+++ kelley_ryan_hello_world7.py
@@ -3,4 +3,8 @@ print("Hello, world!") # Comments don't have to start on the beginning of the li
 print("My name is XYZ and I am in period 7B.")
 print("I like turtles.")
 
+my_age = 42
+print(my_age)
 
+fav_color = input("What is your favorite color?\n")
+print(fav_color)
